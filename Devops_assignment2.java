439ef9dbce118b2e8e6322356817a569fb3d0465
priyanadar <52093178+priyanadar@users.noreply.github.com>
2024-06-01 07:13:28 +0000

    Update Devops_assignment2.java

--- Devops_assignment2.java
+++ Devops_assignment2.java
@@ -1,5 +1,5 @@
-public class Assignment2 {
-    public static void main(String[] args) {
-        System.out.println("Hello, Devops Team!!!"); 
-    }
+public class DevOpsExample {
+  public static void main(String[] args) {
+    System.out.println("Welcome to DevOps.");
+  }
 }
